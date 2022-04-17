1e8b7b8fb5e3e9a31ca09735058545328e556b34
georgi-rm <georgi.rm@gmail.com>
2022-04-17 10:17:12 +0300

    Problem 2 "Weekend Or Working Day" from Lab: Conditional Statements Advanced

---
+++ T03ConditionalStatementsAdvanced/Lab/P02WeekendOrWorkingDay/p02_weekend_or_working_way.js
@@ -0,0 +1,25 @@
+function weekendOrWorkingDay(input) {
+
+    const dayOfWeek = input[0];
+
+    switch (dayOfWeek) {
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            console.log("Working day");
+            break;
+        case "Saturday":
+        case "Sunday":
+            console.log("Weekend");
+            break;
+        default:
+            console.log("Error");
+            break;
+    }
+}
+
+weekendOrWorkingDay(["Monday"]);
+weekendOrWorkingDay(["Sunday"]);
+weekendOrWorkingDay(["April"]);
