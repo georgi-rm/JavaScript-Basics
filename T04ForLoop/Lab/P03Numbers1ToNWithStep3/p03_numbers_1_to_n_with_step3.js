3f109c3a2bcd43ea1383c3119513ccd8ddd15fac
georgi-rm <georgi.rm@gmail.com>
2022-05-20 22:16:08 +0300

    Problem 3 "Numbers 1 To N With Step 3" from Lab: For Loop

---
+++ T04ForLoop/Lab/P03Numbers1ToNWithStep3/p03_numbers_1_to_n_with_step3.js
@@ -0,0 +1,13 @@
+function numbers1ToNWithStep3(input) {
+    const n = Number(input[0]);
+
+    for (let i = 1; i <= n; i += 3) {
+        console.log(i);
+    }
+}
+
+numbers1ToNWithStep3(["10"]);
+console.log("-------------------------");
+numbers1ToNWithStep3(["7"]);
+console.log("-------------------------");
+numbers1ToNWithStep3(["15"]);
