966b36165397be8b3671479d147a361d8fe4bfdd
georgi-rm <georgi.rm@gmail.com>
2022-05-22 21:32:05 +0300

    Problem 5 "Character Sequence" from Lab: For Loop

---
+++ T04ForLoop/Lab/P05CharacterSequence/p05_character_sequence.js
@@ -0,0 +1,12 @@
+function characterSequence(input) {
+    const text = input[0];
+
+    for (let i = 0; i < text.length; i++) {
+        console.log(text[i]);
+
+    }
+}
+
+characterSequence(["softuni"]);
+console.log("-------------------------");
+characterSequence(["ice cream"]);
